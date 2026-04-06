9dab7715978392166fa5ec80490e3cc63c71f116
Oswin Jerome <oswinjeromej@gmail.com>
2026-04-06 21:48:54 +0530

    Design

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
 "use client";
 import { Mr_Bedfort } from "next/font/google";
-import { motion } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
 import Image from "next/image";
 import ourImage from "@/assets/us.jpeg";
+import ourImage2 from "@/assets/us2.png";
 const font = Mr_Bedfort({
   weight: "400",
 });
 
 const Home = () => {
+  const { scrollYProgress } = useScroll();
+  const scaleImage = useTransform(scrollYProgress, [0, 1], [1, 2.5]);
+  const imageRadius = useTransform(scrollYProgress, [0.5, 0], [1, 200]);
+  const widthMargin = useTransform(scrollYProgress, [0.4, 0], [0, 32]);
+
   return (
     <main className="bg-background min-h-screen">
       <section className="pt-8">
@@ -24,12 +30,48 @@ const Home = () => {
           </motion.p>
         </div>
       </section>
-      <section className="w-full flex flex-col justify-center items-center p-8">
-        <motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 1.5 }} className="w-full max-w-3xl xl:w-xl xl:h-md rounded-lg overflow-hidden rounded-t-full ring-offset-3 ring-1">
-          <Image src={ourImage} alt="Our Image" className=" rounded-t-full w-full" />
+      <section className="w-full flex flex-col justify-center items-center">
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          style={{
+            borderTopLeftRadius: imageRadius,
+            borderTopRightRadius: imageRadius,
+
+            margin: widthMargin,
+          }}
+          transition={{ duration: 0.5, delay: 1.5 }}
+          className=" aspect-3/4 xl:aspect-square overflow-hidden ring-offset-3 ring-1 max-w-175 xl:max-w-100 ring-primary"
+        >
+          <motion.img initial={{ scale: 1.05 }} animate={{ scale: 1 }} style={{ scale: scaleImage }} transition={{ duration: 0.5, delay: 1.5 }} src={ourImage2.src} alt="Our Image" className=" w-full object-cover aspect-3/4 xl:aspect-square" />
         </motion.div>
-        <p className="text-center pt-4 ">With great pleasure, we joyously seek the honour of your presence prayers and blessings with family at our wedding!</p>
+        <motion.p initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 2 }} className="text-center pt-4 px-8">
+          With great pleasure, we joyously seek the honour of your presence prayers and blessings with family at our wedding!
+        </motion.p>
       </section>
+      <section className="p-8 mt-10 xl:text-center">
+        <motion.h2 viewport={{ amount: 1 }} initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }} className="text-2xl text-primary font-bold">
+          The Venue
+        </motion.h2>
+        <motion.div viewport={{ amount: 1 }} initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }}>
+          <motion.h3 className="text-4xl text-text font-bold mt-4">Wedding</motion.h3>
+          <p className="mt-2">The Seventh-Day Adventist Church, Water Tank Road, Nagercoil.</p>
+          <a href="#" className="text-primary underline">
+            Get Directions
+          </a>
+        </motion.div>
+        <motion.div viewport={{ amount: 1 }} initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }}>
+          <h3 className="text-4xl text-text font-bold mt-4">Reception</h3>
+          <p className="mt-2">Y R Mahal, Water Tank Road, Nagercoil - 1</p>
+          <a href="#" className="text-primary underline">
+            Get Directions
+          </a>
+        </motion.div>
+        <motion.p viewport={{ amount: 1 }} initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }} className="mt-4 italic">
+          May we gather to witness their union in Christ and celebrate the beginning of their journey together in faith, hope and love.
+        </motion.p>
+      </section>
+      <section className="h-[1200px]"></section>
     </main>
   );
 };
